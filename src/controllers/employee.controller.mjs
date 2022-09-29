b55a078997e99c8f0c5062fdc232310ea0755c36
HungParfait <81432965+HungParfait@users.noreply.github.com>
2022-09-29 10:57:21 +0700

    Employee Controller

--- src/controllers/employee.controller.mjs
+++ src/controllers/employee.controller.mjs
@@ -1,49 +1,92 @@
-export const getEmployeeById = async (req, res) => {
-    try {
-        
+import { query } from "express";
+import createError from "http-errors";
+import Employee from "../models/employees.mjs";
+import { ValidationError } from "sequelize";
 
-    } catch (error) {
-        console.log(error.message);
-        return res.status(500).json("Something is wrong with server")
+export const getEmployee = async (req, res, next) => {
+  const id = req.employeeNumber,
+    role = req.role,
+    { p: page } = req.query,
+    officeCode = req.officeCode,
+    queryFilter = req.query;
+
+  delete queryFilter.p;
+  try {
+    if (role.toLowerCase === "staff") {
+      queryFilter = Object.assign(queryFilter, { employeeNumber: id });
+    } else if (role.toLowerCase === "leader") {
+      queryFilter = Object.assign(queryFilter, { reportsTo: id });
+    } else if (role.toLowerCase === "manager") {
+      queryFilter = Object.assign(queryFilter, { officeCode });
     }
-}
 
-export const addEmployee = async (req, res) => {
-    try {
-     
-    } catch (error) {
-        console.log(error.message);
-        return res.status(400).json("Something is wrong with server")
+    let employeeList = await Employee.findAndCountAll({
+      where: queryFilter,
+      offset: (page || 1 - 1) * 10,
+      limit: 10,
+    });
+
+    return res.status(200).json({
+      employeeList,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const addEmployee = async (req, res, next) => {
+  try {
+    const role = req.role.toLowerCase(),
+      { employee } = req.body;
+
+    if (role === "staff" || role === "manager" || role === "leader") {
+      return next(createError(401, "Not permitted!"));
     }
-}
 
-export const addEmployeeWithCustomerBatch = async (req, res) => {
-    
-    try {
-       
-        
-    } catch (error) {
-        console.log(error.message);
-        return res.status(400).json("Something is wrong with server")
+    let employeeInstance = await Employee.create(employee);
+    return res
+      .status(200)
+      .json({ employeeNumber: employeeInstance.employeeNumber });
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      next(createError(400, "Wrong data!"));
+    } else {
+      next(error);
     }
-}
-
+  }
+};
 
 export const updateEmployee = async (req, res) => {
-    try {
-      
-    } catch (error) {
-        console.log(error.message);
-        return res.status(500).json("Something is wrong with server")
+  try {
+    const role = req.role.toLowerCase(),
+      officeCode = req.officeCode,
+      { employee, employeeID } = req.body;
+
+    if (role === "staff" || role === "leader") {
+      return next(createError(401, "Not permitted!"));
     }
-}
+
+    let queryObj = Object.assign({}, { employeeNumber: employeeID });
+
+    if (role === "manager") {
+      queryObj = Object.assign(queryObj, { officeCode });
+    }
+
+    let employeeInstance = await Employee.update(employee, {
+      where: queryObj,
+    });
+
+    return res
+      .status(200)
+      .json({ employeeNumber: employeeInstance.employeeNumber });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const deleteEmployee = async (req, res) => {
-    try {
-       
-
-    } catch (error) {
-        console.log(error.message);
-        return res.status(500).json("Something is wrong with server")
-    }
-}
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
--- src/main.mjs
+++ src/main.mjs
@@ -1,29 +1,41 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
-import config from './config/config.mjs'
-import connectToDb from './config/database.mjs'
+import config from "./config/config.mjs";
+import connectToDb from "./config/database.mjs";
 
-import customerRouter from './routes/customer.route.mjs'
-import employeeRouter from './routes/employee.route.mjs'
-import loggerRouter from './routes/logger.route.mjs'
-import officeRouter from './routes/offices.route.mjs'
+import customerRouter from "./routes/customer.route.mjs";
+import employeeRouter from "./routes/employee.route.mjs";
+import loggerRouter from "./routes/logger.route.mjs";
+import officeRouter from "./routes/offices.route.mjs";
+import createHttpError from "http-errors";
 
-const app = express()
-const port = config.port || process.env.PORT
+const app = express();
+const port = config.port || process.env.PORT;
 
 connectToDb();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use('/customers', customerRouter)
-app.use('/employees', employeeRouter)
-app.use('/offices', officeRouter)
-app.use('/logger', loggerRouter)
+app.use("/customers", customerRouter);
+app.use("/employees", employeeRouter);
+app.use("/offices", officeRouter);
+app.use("/logger", loggerRouter);
 
+app.use((err, req, res, next) => {
+  if (!err) next(createHttpError(404,'Not found'));
+  else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  // render the error page
+  res.status(err.status || 500).json(err.message);
+});
 
 app.listen(port, () => {
-    console.log(`App connected successfully on port ${port}`)
-})
+  console.log(`App connected successfully on port ${port}`);
+});
