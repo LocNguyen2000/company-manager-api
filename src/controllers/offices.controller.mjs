54d42c6f4f2e030a7ca683577f6126e3625383ff
HungParfait <81432965+HungParfait@users.noreply.github.com>
2022-09-30 09:27:46 +0700

    update

---
+++ src/controllers/offices.controller.mjs
@@ -0,0 +1,103 @@
+import createError from "http-errors";
+import { ValidationError } from "sequelize";
+
+import { ROLE } from "../config/variables.mjs";
+import Employee from "../models/employees.mjs";
+
+
+
+export const getEmployee = async (req, res, next) => {
+  try {
+    const id = req.employeeNumber,
+      role = req.role.toLowerCase(),
+      { p: page } = req.query,
+      officeCode = req.officeCode,
+      queryFilter = req.query;
+
+    if (page) delete queryFilter.p;
+    
+    if (role.toLowerCase === "staff") {
+      queryFilter = Object.assign(queryFilter, { employeeNumber: id });
+    } else if (role === ROLE.LEADER) {
+      queryFilter = Object.assign(queryFilter, { reportsTo: id });
+    } else if (role === ROLE.MANAGER) {
+      queryFilter = Object.assign(queryFilter, { officeCode });
+    }
+
+    let employeeList = await Employee.findAndCountAll({
+      where: queryFilter,
+      offset: ((page || 1) - 1) * 10,
+      limit: 10,
+    });
+
+    return res.status(200).json({data: employeeList});
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const addEmployee = async (req, res, next) => {
+  try {
+    const role = req.role,
+      employee = req.body;
+
+    if (role === ROLE.STAFF || role === ROLE.MANAGER || role === ROLE.LEADER) {
+      return next(createError(401, "Not permitted!"));
+    }
+
+    let employeeInstance = await Employee.create(employee);
+
+    return res.status(200).json({ employeeNumber: employeeInstance.employeeNumber });
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      return next(createError(400, "Wrong data!"));
+    } 
+    return next(error);
+    
+  }
+};
+
+export const updateEmployee = async (req, res) => {
+  try {
+    const role = req.role,
+      officeCode = req.officeCode,
+      employee = req.body,
+      { id } = req.params;
+
+    if (role === ROLE.STAFF || role === ROLE.LEADER) {
+      return next(createError(401, "Not permitted!"));
+    }
+
+    let queryObj = Object.assign({}, { employeeNumber: id });
+
+    if (role === ROLE.MANAGER) {
+      queryObj = Object.assign(queryObj, { officeCode });
+    }
+
+    let employeeInstance = await Employee.update(employee, {
+      where: queryObj,
+    });
+
+    return res.status(200).json({ data: employeeInstance });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteEmployee = async (req, res) => {
+  try {
+    const role = req.role,
+      { id } = req.params;
+
+    if (role === ROLE.STAFF || role === ROLE.MANAGER || role === ROLE.LEADER) {
+      return next(createError(401, "Not permitted!"));
+    }
+
+    let employeeInstance = await Employee.destroy({where: {employeeNumber: id}});
+
+    return res.status(200).json({ employeeNumber: employeeInstance.employeeNumber });
+
+  } catch (error) {
+    next(error);
+  }
+};
--- src/main.mjs
+++ src/main.mjs
@@ -4,11 +4,11 @@ import cors from "cors";
 import config from "./config/config.mjs";
 import connectToDb from "./config/database.mjs";
 
-import customerRouter from './routes/customer.route.mjs'
-import employeeRouter from './routes/employee.route.mjs'
-import loggerRouter from './routes/logger.route.mjs'
-import officeRouter from './routes/offices.route.mjs'
-import userRouter from './routes/auth.router.mjs'
+import customerRouter from "./routes/customer.route.mjs";
+import employeeRouter from "./routes/employee.route.mjs";
+import loggerRouter from "./routes/logger.route.mjs";
+import officeRouter from "./routes/offices.route.mjs";
+import userRouter from "./routes/auth.router.mjs";
 
 const app = express();
 const port = config.port || process.env.PORT;
@@ -19,21 +19,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/users', userRouter)
-app.use('/customers', customerRouter)
-app.use('/employees', employeeRouter)
-app.use('/offices', officeRouter)
-app.use('/logger', loggerRouter)
+app.use("/users", userRouter);
+app.use("/customers", customerRouter);
+app.use("/employees", employeeRouter);
+app.use("/offices", officeRouter);
+app.use("/logger", loggerRouter);
 
 app.use((err, req, res, next) => {
-  if (!err) next(createHttpError(404,'Not found'));
+  if (!err) next(createHttpError(404, "Not found"));
   else {
     next(err);
   }
 });
 
 app.use((err, req, res) => {
-  res.status(err.status || 500).json(err.message);
+  res
+    .status(err.status || 500)
+    .json({ status: err.status || 500, message: err.message });
 });
 
 app.listen(port, () => {
