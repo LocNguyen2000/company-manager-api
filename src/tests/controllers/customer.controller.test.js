ce787b44063c3fbccccdba5964a4ce02de44bbc0
LocNguyen2000 <lolkun2000@gmail.com>
2022-10-03 16:36:02 +0700

    write get unit test & add coverage

--- src/tests/controllers/customer.controller.test.js
+++ src/tests/controllers/customer.controller.test.js
@@ -1,1 +1,47 @@
 import Customer from '../../models/customers.mjs';
+import { getCustomer } from '../../controllers/customer.controller.mjs';
+import { mockCustomersQuery } from '../mocks/customerData.mjs'
+
+let mockRequest, mockResponse, mockNext;
+
+
+describe('Customer controller', () => {
+    describe('get', () => {
+        test('success as president', async () => {
+            mockRequest = {
+                role: 'President',
+                employeeNumber: 1002,
+                customerNumber: null,
+                query: {
+                    customerNumber: 201,
+                    p: 1
+                }
+            }
+            mockResponse = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn().mockReturnThis(),
+            }
+            mockNext = jest.fn()
+            
+            Customer.findAndCountAll = jest.fn().mockResolvedValue(mockCustomersQuery)
+
+            let result = await getCustomer(mockRequest, mockResponse, mockNext);
+
+            expect(result.json.mock.calls[0][0]).toEqual({data : mockCustomersQuery});
+            expect(result.status.mock.calls[0][0]).toEqual(200);
+        })
+        test('success as manager', async () => {
+            
+        })
+
+    })
+    describe('post', () => {
+        
+    })
+    describe('put', () => {
+        
+    })
+    describe('delete', () => {
+        
+    })
+})
