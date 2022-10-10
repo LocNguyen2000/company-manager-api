779e5930c502f65d5740ddb05c106c156c11580c
LocNguyen2000 <lolkun2000@gmail.com>
2022-10-10 11:29:35 +0700

    fix small order controller code

--- src/controllers/orders.controller.mjs
+++ src/controllers/orders.controller.mjs
@@ -36,7 +36,7 @@ export const getOrder = async (req, res, next) => {
             })
 
             if (queryFilter.customerNumber) {
-                if (!staffCustomerNumbers.includes(parseInt(queryFilter.customerNumber))){
+                if (!staffCustomerNumbers.includes(parseInt(queryFilter.customerNumber))) {
                     return next(createError(401, 'Staff can only check their own customers orders'))
                 }
 
@@ -44,7 +44,7 @@ export const getOrder = async (req, res, next) => {
             }
 
             queryFilter = Object.assign(queryFilter, {
-                customerNumber: {[Op.or]: staffCustomerNumbers }
+                customerNumber: { [Op.or]: staffCustomerNumbers }
             })
         }
         // role = leader, manager, president > xem mọi
@@ -62,32 +62,43 @@ export const addOrder = async (req, res, next) => {
     var t = await sequelize.transaction();
 
     try {
-        // CHECK ORDER NUMBER ĐÃ TỒN TẠI (gồm cả soft delete)
-        let isExistRecord = await Order.findByPk(order.orderNumber, { paranoid: false, transaction: t })
-
-        if (!!isExistRecord){
-            throw new ValidationError('This order number already exist')
+         // Request - check details
+         if (!(details instanceof Array)) {
+            throw new ValidationError('Order detail list must be an array')
         }
 
         // Order
-        let orderInstance = await Order.create(
-            Object.assign(order, {
-                orderDate: new Date().toDateString(),
-                updatedBy: username,
-                createdBy: username,
-            }), { transaction: t })
+        order = Object.assign(order, {
+            orderDate: new Date().toDateString(),
+            updatedBy: username,
+            createdBy: username,
+        })
         
-        // STATUS FLOW ERROR
-        if (orderInstance.status != ORDER_STATUS.IN_PROCESS && orderInstance.status != ORDER_STATUS.COD){
+        // Order - CHECK ORDER NUMBER ĐÃ TỒN TẠI (gồm cả soft delete)
+        let [orderInstance, created] = await Order.findOrCreate({ 
+            where: { orderNumber: order.orderNumber }, 
+            defaults: order, 
+            paranoid: false, 
+            raw: true,
+            transaction: t
+        })
+        // Order - nếu model tìm thấy tồn tại order này > instance chưa được tạo > created = false
+        if (!created) {
+            throw new ValidationError('This order number already exist')
+        }
+
+        // Order - STATUS FLOW ERROR
+        if (orderInstance.status != ORDER_STATUS.IN_PROCESS && orderInstance.status != ORDER_STATUS.COD) {
             throw new ValidationError('New order status must be In-Process or COD')
         }
 
-        // Find if customer exist > get credit limit and customer number
-        let customerInstance = await Customer.findByPk(orderInstance.customerNumber, { attributes: ['customerNumber', 'creditLimit'], transaction: t});
-        if (!customerInstance){
+        // Order - Find if customer exist > get credit limit and customer number
+        let customerInstance = await Customer.findByPk(orderInstance.customerNumber, { attributes: ['customerNumber', 'creditLimit'], transaction: t });
+        if (!customerInstance) {
             throw new Error('This customer does not exist')
         }
 
+        // Payment
         payment = Object.assign(payment, {
             checkNumber: orderInstance.orderNumber,
             customerNumber: customerInstance.customerNumber,
@@ -96,26 +107,28 @@ export const addOrder = async (req, res, next) => {
             createdBy: username,
         })
 
-        // Payment
-        let paymentInstance = await Payment.create(payment, {transaction: t});
+        let paymentInstance = await Payment.create(payment, { transaction: t });
 
         // Payment - check if COD > check payment amount customer > credit limit > reject order
-        if (orderInstance.status == ORDER_STATUS.COD){
-            let queryFilter = { customerNumber: customerInstance.customerNumber, status: { [Op.not]: [ORDER_STATUS.SHIPPED, ORDER_STATUS.CANCELLED] }}
-            
+        if (orderInstance.status == ORDER_STATUS.COD) {
+            let queryFilter = { customerNumber: customerInstance.customerNumber, status: { [Op.not]: [ORDER_STATUS.SHIPPED, ORDER_STATUS.CANCELLED] } }
+
             // Find orders status != (CANCELLED & SHIPPED) 
-            let orderWithDetails = await Order.findAll({ raw: true, where: queryFilter, include: [{model: OrderDetail, required: true, attributes: 
-                [
-                    'quantityOrdered',
-                    'priceEach',
-                    [sequelize.literal('(quantityOrdered * priceEach)'), "TotalDetailPayment"],
-                ] 
-            }] ,transaction: t})
+            let orderWithDetails = await Order.findAll({
+                raw: true, where: queryFilter, include: [{
+                    model: OrderDetail, required: true, attributes:
+                        [
+                            'quantityOrdered',
+                            'priceEach',
+                            [sequelize.literal('(quantityOrdered * priceEach)'), "TotalDetailPayment"],
+                        ]
+                }], transaction: t
+            })
 
             // calculate total payment
             let totalPayment = 0;
             for (let order of orderWithDetails) {
-                let payment = order.OrderDetails.reduce((prevValue, currentValue)=> {
+                let payment = order.OrderDetails.reduce((prevValue, currentValue) => {
                     prevValue = currentValue.TotalDetailPayment + prevValue;
 
                     return prevValue;
@@ -125,25 +138,24 @@ export const addOrder = async (req, res, next) => {
             }
 
             // check payment amount customer > credit limit
-            if (totalPayment > customerInstance.creditLimit ){
+            if (totalPayment > customerInstance.creditLimit) {
                 throw new ValidationError('Customer total payment are higher than creditLimit')
             }
         }
 
-        // Order details
-        if (!(details instanceof Array)){
-            throw new ValidationError('Order detail list must be an array')
-        }
+        // Details
+        // Details - Check detail có đủ số sản phẩm tồn tại ko 
         let productCodes = details.map(prd => {
             return prd.productCode
         })
-        let productsInDb = await Product.findAll({where: {productCode: {[Op.in]: productCodes}}, transaction: t})
+        let productsInDb = await Product.findAll({ where: { productCode: { [Op.in]: productCodes } }, transaction: t })
 
-        if (productCodes.length != productsInDb.length){
+        if (productCodes.length != productsInDb.length) {
             throw new ValidationError('Some products does not exist')
         }
 
-        let detailInstances = []; 
+        // Details - Tạo mới
+        let createdDetails = [];
         for (let detail of details) {
             detail = Object.assign(detail, {
                 orderNumber: orderInstance.orderNumber,
@@ -151,10 +163,10 @@ export const addOrder = async (req, res, next) => {
                 createdBy: username,
             })
 
-            let instance = await OrderDetail.create( detail, {transaction: t});
-
-            detailInstances.push(instance);
+            createdDetails.push(detail);
         }
+
+        let detailInstances = await OrderDetail.bulkCreate(createdDetails, { transaction: t, validate : true, });
 
         await t.commit();
 
@@ -177,39 +189,39 @@ export const updateOrder = async (req, res, next) => {
     const username = req.username;
     const { id } = req.params;
     let { comments, status } = req.body;
-    
+
     var t = await sequelize.transaction();
 
-    try {        
-        if (!status){
+    try {
+        if (!status) {
             return res.status(400).json({ message: 'Request body must have status field' })
         }
 
-        let orderInstance = await Order.findByPk(id, {transaction: t, raw: true});
+        let orderInstance = await Order.findByPk(id, { transaction: t, raw: true });
 
-        if (!orderInstance){
+        if (!orderInstance) {
             return res.status(400).json({ message: 'Order not found' })
         }
 
-        if ((orderInstance.status != ORDER_STATUS.IN_PROCESS || (status != ORDER_STATUS.DISPUTED && 
-                                                                status != ORDER_STATUS.SHIPPED && 
-                                                                status != ORDER_STATUS.ON_HOLD))
-            && ( (orderInstance.status != ORDER_STATUS.ON_HOLD) || (status != ORDER_STATUS.SHIPPED))
+        if ((orderInstance.status != ORDER_STATUS.IN_PROCESS || (status != ORDER_STATUS.DISPUTED &&
+            status != ORDER_STATUS.SHIPPED &&
+            status != ORDER_STATUS.ON_HOLD))
+            && ((orderInstance.status != ORDER_STATUS.ON_HOLD) || (status != ORDER_STATUS.SHIPPED))
             && ((orderInstance.status != ORDER_STATUS.DISPUTED) || (status != ORDER_STATUS.RESOLVED))
-            && ( (orderInstance.status != ORDER_STATUS.RESOLVED) || (status != ORDER_STATUS.SHIPPED))
-        ){
+            && ((orderInstance.status != ORDER_STATUS.RESOLVED) || (status != ORDER_STATUS.SHIPPED))
+        ) {
             throw new Error(`Cannot update order from status ${orderInstance.status} to ${status}`)
         }
 
-        orderInstance = Object.assign(orderInstance, { 
+        orderInstance = Object.assign(orderInstance, {
             status,
             updatedBy: username
         })
         orderInstance.shippedDate = status == ORDER_STATUS.SHIPPED ? new Date().toDateString() : orderInstance.shippedDate;
         orderInstance.comments = comments ? comments : orderInstance.comments;
 
-        let rowAffected = await Order.update(orderInstance, {where: { orderNumber: orderInstance.orderNumber, }, transaction: t})
-        
+        let rowAffected = await Order.update(orderInstance, { where: { orderNumber: orderInstance.orderNumber, }, transaction: t })
+
         await t.commit()
         return res.status(200).json({ message: `Update ${rowAffected} order successfully` })
 
@@ -227,7 +239,7 @@ export const deleteOrder = async (req, res, next) => {
 
     try {
         // find order and order details
-        let orderInstance = await Order.findByPk(id, { include: { model: OrderDetail }, transaction: t });
+        let orderInstance = await Order.findByPk(id, { include: { model: OrderDetail }, transaction: t, raw: true });
 
         if (!orderInstance) {
             return next(createError(400, 'Order not found'))
@@ -243,18 +255,18 @@ export const deleteOrder = async (req, res, next) => {
 
         let paymentRowAffected = await Payment.destroy({ where: queryFilter, transaction: t });
 
-        orderInstance = Object.assign(orderInstance.toJSON(), {
+        orderInstance = Object.assign(orderInstance, {
             updatedBy: username,
             status: ORDER_STATUS.CANCELLED,
         })
         orderInstance.comments = comments ? comments : orderInstance.comments;
-        
-        await Order.update(orderInstance,{ where: { orderNumber: orderInstance.orderNumber },transaction: t})
-        
+
+        await Order.update(orderInstance, { where: { orderNumber: orderInstance.orderNumber }, transaction: t })
+
         let orderRowAffected = await Order.destroy({ where: { orderNumber: orderInstance.orderNumber }, transaction: t })
         let detailRowsAffected = await OrderDetail.destroy({ where: { orderNumber: orderInstance.orderNumber }, transaction: t })
         await t.commit();
-        
+
         return res.status(200).json({ message: `Delete ${orderRowAffected} order, ${paymentRowAffected} payment, ${detailRowsAffected} details successfully` })
     } catch (error) {
         await t.rollback()
